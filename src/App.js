53acf708e8de1966fbfa75d624ce9e638092f256
Warley Leandro dos Anjos <warley.anjos@mundiale.com.br>
2021-04-14 12:21:07 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './components/Button';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Pokedex</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
